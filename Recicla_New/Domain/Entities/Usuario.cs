dbc00867931bb80d795676efcdd365eb5db776f9
Rhyzert <leoflorentino97@gmail.com>
2024-11-04 12:05:15 -0300

    main

--- Recicla_New/Domain/Entities/Usuario.cs
+++ Recicla_New/Domain/Entities/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Domain.Entities
     [Table("Usuarios")]
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
