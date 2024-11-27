01ddbf98697dba721e5d4644e0d723e23ba959ce
Rhyzert <leoflorentino97@gmail.com>
2024-11-27 16:19:06 -0300

    .

--- Recicla_New/Infrastructure/Notificador/Interface/INotificador.cs
+++ Recicla_New/Infrastructure/Notificador/Interface/INotificador.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ServiceDomain.Notificador.Interface
+namespace Infrastructure.Notificador.Interface
 {
     public interface INotificador
     {
